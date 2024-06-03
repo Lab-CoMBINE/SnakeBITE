d20869d461b46618c4b7b0f3010c151107bf2731
Andrea Bimbocci <andrea.bimbocci93@gmail.com>
2024-06-03 18:21:36 +0200

    Added a script function to avoid empty flags inside Snakefile command lines

--- workflow/scripts.py
+++ workflow/scripts.py
@@ -6,19 +6,26 @@ def only_flag(param):
     flag = parts[0] if len(parts) > 1 and parts[1] == 'True' else ""
     return flag
 
+def empty_flag(param):
+    parts = param.split(maxsplit=1)
+    if len(parts) == 2 and parts[1].strip():
+        return param
+    else:
+        return ""
+
 def ext_to_folder(file):
     if file.endswith('.pod5'):
         dir_path = os.path.dirname(file)
         return dir_path
 
-def paralelization_func(threads_num):
+def paralelization_func(threads_num, config_file):
     if threads_num == 1:
         return 1
     else:
-        if int(config["paral_ops"]) == 0:
-            return int(config["threads"])
+        if int(config_file["paral_ops"]) == 0:
+            return int(config_file["threads"])
         else:
-            return int(config["paral_threads"])/int(config["paral_ops"])
+            return int(config_file["paral_threads"])/int(config_file["paral_ops"])
 
 def hg_to_GRCh(hg):
     if hg == "hg37":
